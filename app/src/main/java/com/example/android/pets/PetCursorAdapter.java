7506937f1832d10ca4dbfd0cdd1adab7f4ec455b
Patrick Täuber <patrick_marcel.taeuber@fh-bielefeld.de>
2017-04-15 18:54:28 +0200

    Implement cursor adapter

--- app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -2,9 +2,11 @@ package com.example.android.pets;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
 
 /**
  * {@link PetCursorAdapter} is an adapter for a list or grid view
@@ -34,8 +36,9 @@ public class PetCursorAdapter extends CursorAdapter {
      */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        // TODO: Fill out this method and return the list item view (instead of null)
-        return null;
+        // Fill out this method and return the list item view (instead of null)
+        View itemView = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+        return itemView;
     }
 
     /**
@@ -50,6 +53,13 @@ public class PetCursorAdapter extends CursorAdapter {
      */
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        // TODO: Fill out this method
+        TextView tvName = (TextView) view.findViewById(R.id.name);
+        TextView tvBreed = (TextView) view.findViewById(R.id.summary);
+
+        String name = cursor.getString(cursor.getColumnIndexOrThrow("name"));
+        String breed = cursor.getString(cursor.getColumnIndexOrThrow("breed"));
+
+        tvName.setText(name);
+        tvBreed.setText(breed);
     }
 }
